c00bee634422b656b2618b9603673191f0891a2f
Liam Walsh <liw6800@gmail.com>
2020-11-19 21:32:17 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -46,12 +46,12 @@ end
     end
   end
 
-  if position_taken?(board, index) == false && index.between?(0,8) == true
-    return true
-  else
-    return false
-  end
-end
+  #if position_taken?(board, index) == false && index.between?(0,8) == true
+  #  return true
+  #else
+  #  return false
+#  end
+#end
 
 def turn_count(board)
   turns = 0
